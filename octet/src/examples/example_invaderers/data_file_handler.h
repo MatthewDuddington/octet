3fe30e47d80c8855a9dca63e0057ed3f798a50b9
Matthew <mdudd001@gold.ac.uk>
2016-10-16 13:42:16 +0100

    Level file loading progress
    Need to stop writing all this first... get it working beforehand!

--- octet/src/examples/example_invaderers/data_file_handler.h
+++ octet/src/examples/example_invaderers/data_file_handler.h
@@ -10,8 +10,7 @@ class data_file_handler {
 
   void readDataFile(std::string file_location,
                     std::vector<std::string> data_string_vectors[], // TODO Could this be more generic by having a pointer array to a set of different vector types based on passed argument type - the problem I'm facing is that we have to declair the pointer types explicitly as C++ doesn't have generics? For now I'm storing everything as strings and the calling code can handle translating them into the appropriate expected format.
-                    char data_seperator
-                   ) {
+                    char data_seperator) {
     // Open the CSV file
     std::ifstream myFile(file_location);
 
--- octet/src/examples/example_invaderers/level_file_handler.h
+++ octet/src/examples/example_invaderers/level_file_handler.h
@@ -1,3 +1,8 @@
+/*
+Handles the parsing of custom level design files.
+Assumes that the file has been prepared to the same size as the array.
+*/
+
 #ifndef level_file_handler_h
 #define level_file_handler_h
 
@@ -7,6 +12,53 @@
 
 class level_file_handler {
   
+  std::vector<char> file_contents;
+
+  void extract_file_content(const std::string &file_location) {
+    std::ifstream inputFile(file_location);
+
+    if (inputFile.bad) {
+      printf("%s", "ERROR: File was not loaded succesfully.");
+    }
+    else {
+      // Load the chars into file_contents
+    }
+  }
+
+public:
+
+  // Loads the level file only once and returns the char.
+  // Use in a level loading loop like the space invaiderers array.
+  char get_design_symbol(int symbol_index,
+                         const std::string &file_location) {
+    if (file_contents.empty) {
+      extract_file_content(file_location);
+    }
+    return get_design_symbol(symbol_index);
+  }
+
+  char get_design_symbol(int symbol_index) {
+    if (file_contents.empty) {
+      printf("%s", "ERROR: No level design file has been loaded.");
+      return NULL;
+    }
+    if (file_contents.size() < symbol_index) {
+      printf("%s", "ERROR: Index requested is out of range for the loaded level design.");
+      return NULL;
+    }
+    else {
+      if (file_contents.at(symbol_index) == NULL) {
+        printf("%s", "End of file. Level design file read complete. Clearing file content cache and destructing file handler.");
+        delete[] this;
+      }
+      return file_contents.at(symbol_index);
+    }
+  }
+
+  ~level_file_handler() {
+    file_contents.clear();
+  }
+
   // Function: Private: Extract file content
   // Read in the level design file
   // Store it in a char vector
