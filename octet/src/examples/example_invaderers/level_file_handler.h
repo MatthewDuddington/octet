a87132acdbf1c566443974f3ac06441872186b48
Matthew <mdudd001@gold.ac.uk>
2016-11-07 02:13:37 +0000

    Unstable flood design

--- octet/src/examples/example_invaderers/level_file_handler.h
+++ octet/src/examples/example_invaderers/level_file_handler.h
@@ -53,34 +53,43 @@ namespace octet {
       // Controls how many obstacles appear in the level.
       int obstical_density = 0.4f;
       
+      // Decide how big the map should be.
       vec2 random_xy = RandomXY( 15, 15, 7, 7 );
       level_width = random_xy.x;
       level_height = random_xy.y;
-      vec2 start_position = RandomXY((int)level_width / 4, (int)level_height / 4); // Place start in top left corner somewhere
+      // Place start in top left corner somewhere
+      vec2 start_position = RandomXY((int)level_width / 4, (int)level_height / 4); 
+      // Place goal somewhere near the bottom
+      vec2 goal_position = RandomXY((int)level_width, (int)level_height / 4);
+      goal_position.y() += (int)(level_height * 3) / 4;
 
       // 2 dimensional array of bools to represent possible obstacle placement.
       std::vector< std::vector<bool> > proxy_map( level_height, std::vector<bool>(level_width) );
-      std::vector< std::vector<bool> > check_map( level_height, std::vector<bool>(level_width) );
+      //std::vector< std::vector<bool> > check_map( level_height, std::vector<bool>(level_width) );
 
+      // Decide the total number of obsticals we should have.
       int number_of_obsticals = (int)(level_width * level_height * obstical_density);
+
       int current_placed_obsticals = 0;
-      for (int i = 0; i < number_of_obsticals; i++)
+      for ( int i = 0; i < number_of_obsticals; i++ )
       {
         random_xy = RandomXY(level_width - 2, level_height - 2);
-        check_map[random_xy.x][random_xy.y] = true;
+        proxy_map[random_xy.x][random_xy.y] = true;
         current_placed_obsticals++;
 
-        if ( random_xy.get() != start_position.get()
-          && map_can_be_walked(check_map, current_placed_obsticals, level_width - 2, level_height - 2, start_position) )
-        { proxy_map = check_map; }
-        else {
-          check_map[random_xy.x][random_xy.y] = false;
+        if ( random_xy.get() == start_position.get()
+          || random_xy.get() == goal_position.get()
+          || !map_can_be_walked( proxy_map
+                               , current_placed_obsticals
+                               , level_width - 2, level_height - 2
+                               , start_position) )
+        {
+          proxy_map[random_xy.x][random_xy.y] = false;
           current_placed_obsticals--;
         }
       }
 
-      // create random map
-
+      // Create the map
       for ( int row = 0; row < level_height; row++ ) 
         // Wall off top and bottom rows.
         if ( row == 0 
@@ -94,19 +103,68 @@ namespace octet {
             if ( colum == 0
               || colum == level_width - 1 )
             { level_design_.push_back('x'); }
-            else { return; }
+            else 
+            {
+              if ( start_position.x() == row
+                && start_position.y() == colum)
+              { level_design_.push_back('S'); }
+              else if ( goal_position.x() == row
+                     && goal_position.y() == colum)
+              { level_design_.push_back('G'); }
+              else if (proxy_map[row][colum]) {
+                level_design_.push_back('b'); 
+              }
+            }
           }
         }
     }
 
-    bool map_can_be_walked( std::vector< std::vector<bool> > check_map
+    // Algorithm adapted from https://www.youtube.com/watch?v=q7BL-lboRXo
+    bool map_can_be_walked( std::vector< std::vector<bool> > proxy_map
       ,                     int total_acceptable_obsticals
       ,                     int width, int height
       ,                     vec2 start_position ) 
     {
-      std::vector< std::vector<bool> > map_flags(height, std::vector<bool>(width));
+      std::vector< std::vector<bool> > checked_coords(height, std::vector<bool>(width));
       std::vector<vec2> queue;
-      queue.push_back()
+      queue.push_back(start_position);
+      checked_coords[start_position.x()][start_position.y()] = true;
+
+      int walkable_cell_count = 1;
+
+      while (queue.size > 0) {
+        vec2 coord = queue.at(queue.size()-1); // Get last item...
+        queue.pop_back();  // ...and remove it from the queue.
+
+        for (int x = -1; x <= 1; x++)    // For each of three x positions...
+        {    
+          for (int y = -1; y <= 1; y++)  // ...in each of the three y positions.
+          {  
+            int neighbour_x = coord.x() + x;
+            int neighbour_y = coord.y() + y;
+            if (x == 0 || y == 0)  // But ignoring diagonals.
+            { 
+              // Make sure the coords are not in the outer wall.
+              if ( neighbour_x >= 0
+                && neighbour_x < width
+                && neighbour_y >= 0
+                && neighbour_y < height )
+              {
+                if ( !checked_coords[neighbour_x][neighbour_y]  // Ignore if already checked.
+                  && !proxy_map[neighbour_x][neighbour_y] )     // Block progress if obstacle.
+                {
+                  checked_coords[neighbour_x][neighbour_y] = true;  // Mark that coords have been checked.
+                  queue.push_back(vec2(neighbour_x, neighbour_y));
+                  walkable_cell_count++;
+                }
+              }
+            }
+          }
+        }
+
+        int target_walkable_cells = (int)(width * height - total_acceptable_obsticals);
+        return target_walkable_cells == walkable_cell_count; // Answers whether we accessed all tiles that didn't have obsticles on them.
+      }
     }
 
 
